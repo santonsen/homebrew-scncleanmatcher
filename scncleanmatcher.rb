3ab75f960ccac5032ba95b97ff5ebd23b821969d
Sebastian Antonsen <sebastian@stormstudios.no>
2015-01-21 15:18:36 +0100

    created 0.0.3 distribution

--- scncleanmatcher.rb
+++ scncleanmatcher.rb
@@ -3,7 +3,7 @@ require "formula"
 class Scnscrapematcher < Formula
   homepage "http://shortcutolso.no"
   url "https://github.com/santonsen/homebrew-scnprojectsetup/raw/master/dist/scnscrapematcher_0.0.2/scnscrapematcher_0.0.2.tar.gz"
-  sha1 "0e407465ea16c9f9731365a9b5733d7ee1cdd577"
+  sha1 "f45ccc533b19846b996afca129cb1d7e4e3bce4b"
 
   def install
     # Remove unrecognized options if warned by configure
