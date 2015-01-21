bb69538057f5c6fa7312cfaecb6dbb8a0ea931d6
Sebastian Antonsen <sebastian@stormstudios.no>
2015-01-21 15:39:34 +0100

    and now the version number is correct in the ruby file as well

--- scncleanmatcher.rb
+++ scncleanmatcher.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Scncleanmatcher < Formula
   homepage "http://shortcutolso.no"
-  url "https://github.com/santonsen/homebrew-scncleanmatcher/raw/master/dist/scncleanmatcher_0.0.2/scncleanmatcher_0.0.2.tar.gz"
-  sha1 "a4e6fd86bc898a5c794f03b3427d2c91cd91dae5"
+  url "https://github.com/santonsen/homebrew-scncleanmatcher/raw/master/dist/scncleanmatcher_0.0.4/scncleanmatcher_0.0.4.tar.gz"
+  sha1 "bb4b34392be15a60b9935bfe909271684a971d0b"
 
   def install
     # Remove unrecognized options if warned by configure
