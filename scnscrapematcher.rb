9c366ef2703407add67020030393930efb037089
Sebastian Antonsen <sebastian@stormstudios.no>
2015-01-21 15:12:49 +0100

    first commit. Make and ruby might need some tweaks

---
+++ scnscrapematcher.rb
@@ -0,0 +1,13 @@
+require "formula"
+
+class Scnscrapematcher < Formula
+  homepage "http://shortcutolso.no"
+  url "https://github.com/santonsen/homebrew-scnprojectsetup/raw/master/dist/scnprojectsetup_1.2.1/scnprojectsetup_1.2.1.tar.gz"
+  sha1 "2d5a7ad530abe71354cce343abd337231bd87567"
+
+  def install
+    # Remove unrecognized options if warned by configure
+    system "make", "prefix=#{prefix}",
+           "install"
+  end
+end
