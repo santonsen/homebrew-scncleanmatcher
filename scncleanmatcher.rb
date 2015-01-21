f16acf0b0481cd29f2c734838bc04abd23fb8c3c
Sebastian Antonsen <sebastian@stormstudios.no>
2015-01-21 16:37:00 +0100

    upped to 0.0.6

--- scncleanmatcher.rb
+++ scncleanmatcher.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Scncleanmatcher < Formula
   homepage "http://shortcutolso.no"
-  url "https://github.com/santonsen/homebrew-scncleanmatcher/raw/master/dist/scncleanmatcher_0.0.5/scncleanmatcher_0.0.5.tar.gz"
-  sha1 "89652be51ce402c64cbf9c20976ad7ab24bb9efb"
+  url "https://github.com/santonsen/homebrew-scncleanmatcher/raw/master/dist/scncleanmatcher_0.0.6/scncleanmatcher_0.0.6.tar.gz"
+  sha1 "1ddc3be64ddde09a9691c21231974fe2bc027273"
 
   def install
     # Remove unrecognized options if warned by configure
