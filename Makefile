1250388ca5892162c3a1d58de3d815f3815827ee
Sebastian Antonsen <sebastian@stormstudios.no>
2015-01-23 13:36:44 +0100

    matching works better now upped to 0.0.7

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 NAME = scncleanmatcher
-VERSION = 0.0.6
+VERSION = 0.0.7
 MAINTAINER = Sebastian Antonsen <sebastian@stormstudios.no>
 
 BASE_DIR=dist
--- scncleanmatcher.rb
+++ scncleanmatcher.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Scncleanmatcher < Formula
   homepage "http://shortcutolso.no"
-  url "https://github.com/santonsen/homebrew-scncleanmatcher/raw/master/dist/scncleanmatcher_0.0.6/scncleanmatcher_0.0.6.tar.gz"
-  sha1 "1ddc3be64ddde09a9691c21231974fe2bc027273"
+  url "https://github.com/santonsen/homebrew-scncleanmatcher/raw/master/dist/scncleanmatcher_0.0.7/scncleanmatcher_0.0.7.tar.gz"
+  sha1 "3907655508da2e26b9a800a7f9d8e4b0d7e8f135"
 
   def install
     # Remove unrecognized options if warned by configure
