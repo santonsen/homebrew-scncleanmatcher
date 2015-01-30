ca783184edb3806a4d4c08562f04a32629ccc676
Sebastian Antonsen <sebastian@stormstudios.no>
2015-01-30 11:29:41 +0100

    uses .* when on matchstring. This allows for matches where the clean part and the dirty part are of different lengths.
    It also replaces any instances of '_mute_' with .*
    This allows for clean files with no audio to be matched
    with masters with audio that are otherwise identical.
    
    upped to 1.1.0

--- scncleanmatcher.rb
+++ scncleanmatcher.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Scncleanmatcher < Formula
   homepage "http://shortcutolso.no"
-  url "https://github.com/santonsen/homebrew-scncleanmatcher/raw/master/dist/scncleanmatcher_1.0.0/scncleanmatcher_1.0.0.tar.gz"
-  sha1 "20a6bd0eb6e404ffdf6c7bb1f31c84c646a00ab6"
+  url "https://github.com/santonsen/homebrew-scncleanmatcher/raw/master/dist/scncleanmatcher_1.1.0/scncleanmatcher_1.1.0.tar.gz"
+  sha1 "a45269c3a4e9e1df5fee45ef0fff574395633f10"
 
   def install
     # Remove unrecognized options if warned by configure
